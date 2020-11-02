43e3a315fdcbdc96bc6d0c390ea0b45572fc0a02
dani.camp <bboydanee@gmail.com>
2020-11-02 03:24:29 +0000

    section 14

---
+++ app/controllers/api/v1/to_dos_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::ToDosController < ActionController::API
+    def index
+      @to_dos = ToDo.all
+      render json: to_dos
+    end
+    
+    def destroy
+      to_do = ToDo.find_by(id: params[:id])
+      to_do.destroy
+    end
+end
